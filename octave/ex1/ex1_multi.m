1d8855f24a64fd6fa2b7afe2cb51d9d778350fdc
Tung Duong Bui <tdbui1209@gmail.com>
2021-08-04 18:57:58 +0700

    normalize test features for grad descent multi

--- octave/ex1/ex1_multi.m
+++ octave/ex1/ex1_multi.m
@@ -104,7 +104,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = [1 1650 3] * theta; % You should change this
+a = ([1650 3] - mu) ./ sigma
+price = [1 a]  * theta; % You should change this
 
 
 % ============================================================
@@ -156,4 +157,3 @@ price = [1 1650 3] * theta; % You should change this
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using normal equations):\n $%f\n'], price);
-
